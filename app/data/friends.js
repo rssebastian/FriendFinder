341f5575b883440841d0ffbc6cfdfdb1df29a662
Ryan Sebastian <Ryan Sebastian>
2017-08-02 23:39:45 -0700

    working

---
+++ app/data/friends.js
@@ -0,0 +1,46 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the reserved tables.
+// Initially we just set it equal to a "dummy" customer.
+// But you could have it be an empty array as well.
+// ===============================================================================
+
+var friendsArray = [
+	{
+		name: "Max Yeo",
+		photo: "https://scontent-sjc2-1.xx.fbcdn.net/v/t1.0-9/18582235_10213194540160720_8846389105207291503_n.jpg?oh=9ca8e29cd86026be80dc84476ed5a37e&oe=5A37D3BC",
+		scores: [ 3, 3, 4, 2, 4, 3, 3, 4, 4, 4 ]
+	},
+	{
+		name: "Camilo Espinosa",
+		photo: "https://scontent-sjc2-1.xx.fbcdn.net/v/t1.0-9/17309308_10154295652085785_6829868726914459783_n.jpg?oh=77bbdae8f97957e3194498ba463cbb40&oe=59F16019",
+		scores: [ 4, 3, 5, 1, 4, 1, 2, 1, 3, 5 ]
+	},
+	{
+		name: "Casey Pao",
+		photo: "https://scontent-sjc2-1.xx.fbcdn.net/v/t1.0-9/17424713_1348928868490113_2814433560363179355_n.jpg?oh=ea226b6a6451acaa92926bef2af56adb&oe=5A3991AB",
+		scores: [ 4, 3, 3, 2, 2, 2, 3, 4, 3, 3 ]
+	},
+	{
+		name: "Cari Gan",
+		photo: "https://scontent-sjc2-1.xx.fbcdn.net/v/t1.0-9/16002856_10211992916529205_8588337234804414459_n.jpg?oh=4c42127abf02ee555fb3f6caabc94381&oe=59F32333",
+		scores: [ 2, 4, 2, 3, 2, 2, 2, 3, 3, 2 ]
+	},
+	{
+		name: "Charles Lam",
+		photo: "https://scontent-sjc2-1.xx.fbcdn.net/v/t1.0-9/17362367_10154971607665943_8752863357269378167_n.jpg?oh=4499106557255dbe0626fa651c740361&oe=5A3853BB",
+		scores: [ 3, 3, 4, 2, 4, 3, 3, 4, 4, 4 ]
+	},
+	{
+		name: "Mi Le",
+		photo: "https://scontent-sjc2-1.xx.fbcdn.net/v/t1.0-9/17634473_1469032093141588_5052823934022761841_n.jpg?oh=5119394cab39c780478bddcdd32e17aa&oe=5A32E2CD",
+		scores: [ 3, 4, 2, 3, 4, 2, 2, 4, 2, 3 ]
+	},
+	{
+		name: "Shannon Voong",
+		photo: "https://scontent-sjc2-1.xx.fbcdn.net/v/t1.0-9/16427730_10210448581282442_7473091743793991662_n.jpg?oh=8fd4e58c9edd300f1a4823e9c7ae33b0&oe=59EBCB45",
+		scores: [ 5, 2, 3, 1, 4, 1, 4, 1, 4, 5 ]
+	}
+]
+
+module.exports = friendsArray;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,80 @@
+var friendData = require("../data/friends");
+
+module.exports = function(app) {
+	var differences = 0;
+	var leastDifferences, leastDifferencesIndex, bestFriend;
+	
+	app.get("/api/friends", function (req, res) {
+		res.json(friendData);
+	});
+
+	app.post("/api/friends", function(req, res) {
+		var friendDifferences = [];
+		
+		// for (var h=0; h<friendData.length; h++) {
+		// 	if (friendData[h].name === req.body.name) {
+		// 		var newFriendData = friendData.splice(h,1);
+		// 	}
+
+		// 	console.log(friendData[h].name === req.body.name);
+		// }
+
+		// console.log(newFriendData);
+
+		if (newFriendData) {
+			//loops through each friend stored
+			for (var i=0; i<newFriendData.length; i++) {
+				//for each friend it loops and calculates differences between the friend and user
+				for (var j=0; j<newFriendData[i].scores.length; j++) {
+					differences += Math.abs(newFriendData[i].scores[j]-req.body.scores[j]);
+				}
+
+				friendDifferences.push(differences);
+				differences = 0;
+			}
+
+			leastDifferences = friendDifferences[0];
+			leastDifferencesIndex = 0;
+
+			//loop through  and find the friend 
+			for (var k=1; k<friendDifferences.length; k++) {
+				if (friendDifferences[k]<leastDifferences) {
+					leastDifferencesIndex = k;
+					leastDifferences = friendDifferences[k];
+				}
+			}
+
+			newFriendData.push(req.body);
+
+			bestFriend = newFriendData[leastDifferencesIndex];
+			res.json(bestFriend);
+		} else {
+			//loops through each friend stored
+			for (var i=0; i<friendData.length; i++) {
+				//for each friend it loops and calculates differences between the friend and user
+				for (var j=0; j<friendData[i].scores.length; j++) {
+					differences += Math.abs(friendData[i].scores[j]-req.body.scores[j]);
+				}
+
+				friendDifferences.push(differences);
+				differences = 0;
+			}
+
+			leastDifferences = friendDifferences[0];
+			leastDifferencesIndex = 0;
+
+			//loop through  and find the friend 
+			for (var k=1; k<friendDifferences.length; k++) {
+				if (friendDifferences[k]<leastDifferences) {
+					leastDifferencesIndex = k;
+					leastDifferences = friendDifferences[k];
+				}
+			}
+
+			friendData.push(req.body);
+
+			bestFriend = friendData[leastDifferencesIndex];
+			res.json(bestFriend);
+		}
+	});
+};
